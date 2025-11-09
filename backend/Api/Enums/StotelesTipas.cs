12b6417a1d91265b4d88fc02ef1b7f7f6774ebcd
jakuemaa <151637790+jakuemaa@users.noreply.github.com>
2025-11-10 01:12:32 +0200

    commented the enumerators for now, since it needs more implementation and chenges in the controllers and other places

--- backend/Api/Enums/StotelesTipas.cs
+++ backend/Api/Enums/StotelesTipas.cs
@@ -1,9 +1,9 @@
-namespace Api.Enums
-{
-    public enum StotelesTipas
-    {
-        Pradzios = 1,
-        Tarpine = 2,
-        Pabaigos = 3
-    }
-}
+// namespace Api.Enums
+// {
+//     public enum StotelesTipas
+//     {
+//         Pradzios = 1,
+//         Tarpine = 2,
+//         Pabaigos = 3
+//     }
+// }
--- backend/Api/Models/Stotele.cs
+++ backend/Api/Models/Stotele.cs
@@ -29,7 +29,7 @@ namespace Api.Models
         [Required]
         [MaxLength(20)]
         [Column("tipas")]
-        public StotelesTipas Tipas { get; set; } // Pradzios, Tarpine, Pabaigos
+        public string Tipas { get; set; } = string.Empty; // Pradzios, Tarpine, Pabaigos
 
         // Navigation properties
         public ICollection<MarstrutoStotele> MarstrutoStoteles { get; set; } = new List<MarstrutoStotele>();
